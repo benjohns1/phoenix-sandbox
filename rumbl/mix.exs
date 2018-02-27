7151fdcad87b282146f98b740198055f2df73bac
Ben Johns <521959+benjohns1@users.noreply.github.com>
2018-02-27 16:17:33 -0700

    ch11 OTP info system to retrieve API queries based on annotations

--- rumbl/mix.exs
+++ rumbl/mix.exs
@@ -30,16 +30,19 @@ defmodule Rumbl.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.5"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 4.0"},
-     {:bcrypt_elixir, "~> 0.12"}]
+    [
+      {:phoenix, "~> 1.2.5"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 4.0"},
+      {:bcrypt_elixir, "~> 0.12"},
+      {:sweet_xml, "~> 0.5.0"},
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
